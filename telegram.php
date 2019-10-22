256557da3b32a45f87e565700ccfee78d2b55621
Chipi <33845091+ko-ro@users.noreply.github.com>
2019-10-22 16:02:03 +0300

    changes

---
+++ telegram.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['user-name'];
+$surname = $_POST['user-surname']
+$email = $_POST['user-email'];
+$telegram = $_POST['user-telegram']
+$message = $_POST['user-message']
+
+$token = "949565640:AAEGoYzcWtY0kC3MTI0KNfdkWFgxVe8NOQs";
+$chat_id = "-391511120";
+
+$arr = array(
+  'name' => $name,
+  'surname' => $surname,
+  'email' => $email,
+  'telegram' => $telegram,
+  'message' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
