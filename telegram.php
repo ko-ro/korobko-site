bbb39349cea961e9257dba5acb12392ec2f71635
Chipi <33845091+ko-ro@users.noreply.github.com>
2019-10-27 12:12:43 +0200

    fixed

--- telegram.php
+++ telegram.php
@@ -1,19 +1,19 @@
 <?php
 $name = $_POST['user-name'];
-$surname = $_POST['user-surname']
+$surname = $_POST['user-surname'];
 $email = $_POST['user-email'];
-$telegram = $_POST['user-telegram']
-$message = $_POST['user-message']
+$telegram = $_POST['user-telegram'];
+$message = $_POST['user-message'];
 
 $token = "949565640:AAEGoYzcWtY0kC3MTI0KNfdkWFgxVe8NOQs";
 $chat_id = "-391511120";
 
 $arr = array(
-  'name' => $name,
-  'surname' => $surname,
-  'email' => $email,
-  'telegram' => $telegram,
-  'message' => $message
+  'name : ' => $name,
+  'surname : ' => $surname,
+  'email : ' => $email,
+  'telegram : ' => $telegram,
+  'message : ' => $message
 );
 
 foreach($arr as $key => $value) {
@@ -21,3 +21,8 @@ foreach($arr as $key => $value) {
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo "Error";
+}
